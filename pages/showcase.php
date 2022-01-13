dafd97c9657e598ca38f61f1f0397aba5e270c1d
mamstha <86461385+mamstha@users.noreply.github.com>
2022-01-13 14:56:47 +0545

    metro_coffee_1

--- pages/showcase.php
+++ pages/showcase.php
@@ -24,56 +24,57 @@
 
 <div class="container">
     <div class="row">
-        <div id="slides" class="col-12 carousel slide" data-ride="carousel" >
-            <ul class="carousel-indicators">
-                <li data-target="#slides" data-slide-to="0" class="active"></li>
-                <li data-target="#slides" data-slide-to="1"></li>
-                <li data-target="#slides" data-slide-to="2"></li>
-            </ul>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <div class="first-slide-carousel-img">
-            <img src="img/background-coffee.png" alt="">
+                <h2>Serving You Ethically Sourced,<br> Freshly Brewed Coffee, Every Day</h2>
+                
             </div>
-            <div class="first-slide-overlay"></div>                        
-                    
-                <div>
-                <a href="#" class="carousel-control-next" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                        <a href="#" class="carousel-control-prev" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                </div>
-                <div class="carousel-caption text-center">
-                    <h2>Serving You Ethically Sourced,<br> Freshly Brewed Coffee, Every Day</h2>
-                    <div class="button text-center">
-                        <div class="form-group">
-                            <input type="email" class="form-control" id="emailInput" placeholder="Email">
-                        </div>
-                        <div>
-                            <button class="btn btn-default" type="button">
-                                <img src="img/credit-card.png" alt="" class="btn-img">
-                                Become a member
-                            </button>
-                        </div>
-                        <div class="scroll">
-                            <div>
-                                <p> <span class="bold">Scroll Down</span></b> <br> To Discover More<p>
-                                
-                            </div>
-                           <div>
-                            <img src="img/scroll-down 1.png" alt="" class="scroll-img">
-                           </div>
-                        </div>
-                    </div>   
-                </div>
+            <div class="carousel-item">
+                <h2>True love these days can be found in a lot of creative places</h2>
             </div>
+            <div class="carousel-item">
+                <h2>Whether you enjoy a cold brew, a latte, or a double shot of espresso</h2>
+            </div>
+            <div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>           
         </div>
-</div>
+
+        <div class="button text-center">
+            <div class="form-group">
+                <input type="email" class="form-control" id="emailInput" placeholder="Email">
+            </div>
+            <div>
+                <button class="btn btn-default" type="button">
+                    <img src="img/credit-card.png" alt="" class="btn-img">
+                    Become a member
+                </button>
+            </div>
+            
+        </div> 
+
+        <div class="scroll">
+            <div>
+                <p> <span class="bold">Scroll Down</span></b> <br> To Discover More<p>
+                
+            </div>
+        <div>
+            <img src="img/scroll-down 1.png" alt="" class="scroll-img">
+        </div>
+        </div>
+        
     </div>
 </div>
-
-
